7a8827c30063376c208aeff781076e1dc1647ec2
Ali Taqi <alitaqi94.developer@gmail.com>
2024-03-31 14:28:17 +0100

    minor bug fix in .js

--- src/script/designs.js
+++ src/script/designs.js
@@ -1,6 +1,6 @@
 /* Javascript app */
 
-/* Part 1: default dimensions/colour initially */
-document.querySelector("in-vert-pix").value = '1';
-document.querySelector("in-hori-pix").value = '1';
-document.querySelector("colour").value = "#000000"; 
+/* default dimensions/colour initially */
+document.querySelector("#in-vert-pix").value = '1';
+document.querySelector("#in-hori-pix").value = '1';
+document.querySelector("#colour").value = "#000000";
